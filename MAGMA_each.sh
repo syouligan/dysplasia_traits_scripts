f1e8cd1e900640ac5dc732c91d35ecae656e0bbe
syouligan <syoulten@gmail.com>
2020-03-11 15:06:32 -0600

    Submit MAGMA scripts edited

--- MAGMA_each.sh
+++ MAGMA_each.sh
@@ -3,7 +3,7 @@
 # Run MAGMA
 # usage: writes to sample input directory
 # ./MAGMA_each.sh (workDir) (inFile) (geneLoc) (trait) (genome) (geneList)
-# eg ./MAGMA_each.sh /share/ScratchGeneral/scoyou/projects/dysplasia_GWAS/skeletal_traits/UKBB_eBMD Biobank2-British-Bmd-As-C-Gwas-SumStats_MAGMA.input.txt /share/ScratchGeneral/scoyou/projects/dysplasia_GWAS/MAGMA_files/NCBI37/NCBI37.3.gene.loc UKBB_eBMD /share/ScratchGeneral/scoyou/projects/dysplasia_GWAS/MAGMA_files/g1000_eur/g1000_eur /share/ScratchGeneral/scoyou/projects/dysplasia_GWAS/gene_sets/Dysplasia_ENTREZ.txt
+# eg ./MAGMA_each.sh /share/ScratchGeneral/scoyou/projects/dysplasia_GWAS/skeletal_traits Biobank2-British-Bmd-As-C-Gwas-SumStats_MAGMA.input.txt /share/ScratchGeneral/scoyou/projects/dysplasia_GWAS/MAGMA_files/NCBI37/NCBI37.3.gene.loc UKBB_eBMD /share/ScratchGeneral/scoyou/projects/dysplasia_GWAS/MAGMA_files/g1000_eur/g1000_eur /share/ScratchGeneral/scoyou/projects/dysplasia_GWAS/gene_sets/Dysplasia_ENTREZ.txt
 
 # input: gzipped fastq file read1 [read2 for paired-end] 
 #        STAR genome directory, RSEM reference directory - prepared with STAR_RSEM_prep.sh script
@@ -15,13 +15,13 @@ genome=$5 # genome LD files such as those provided with magma, note ancestry spe
 geneList=$6 # a file containing lists of ENTREZ gene IDs for gene set testing (ie SET1 1546 90122 386 6001 23019)
 
 # Creates gene annotation
-/share/ScratchGeneral/scoyou/local/bin/MAGMA/magma --annotate window=1 --snp-loc $workDir/$inFile --gene-loc $geneLoc --out $workDir/$trait
+/share/ScratchGeneral/scoyou/local/bin/MAGMA/magma --annotate window=1 --snp-loc $workDir/$trait/$inFile --gene-loc $geneLoc --out $workDir/$trait
 
 # Calculates genewise enrichment using mean method
-/share/ScratchGeneral/scoyou/local/bin/MAGMA/magma --bfile $genome --gene-annot $workDir/*.genes.annot --gene-model snp-wise=mean --seed 1 --pval $workDir/*_MAGMA.input.txt use=1,4 ncol=5 --out $workDir/$trait'.mean'
+/share/ScratchGeneral/scoyou/local/bin/MAGMA/magma --bfile $genome --gene-annot $workDir/*.genes.annot --gene-model snp-wise=mean --seed 1 --pval $workDir/$trait/$inFile use=1,4 ncol=5 --out $workDir/$trait/$trait'.mean'
 
 # Calculates genewise enrichment using multi method
-/share/ScratchGeneral/scoyou/local/bin/MAGMA/magma --bfile $genome --gene-annot $workDir/*.genes.annot --gene-model snp-wise=multi --seed 1 --pval $workDir/*_MAGMA.input.txt use=1,4 ncol=5 --out $workDir/$trait'.multi'
+/share/ScratchGeneral/scoyou/local/bin/MAGMA/magma --bfile $genome --gene-annot $workDir/*.genes.annot --gene-model snp-wise=multi --seed 1 --pval $workDir/$trait/$inFile use=1,4 ncol=5 --out $workDir/$trait/$trait'.multi'
 
 # Calculates geneset enrichment using mean method
 /share/ScratchGeneral/scoyou/local/bin/MAGMA/magma --gene-results $workDir/*_mean.genes.raw --set-annot $geneList --out $workDir/$trait'.mean'
---
+++ MAGMA_submit.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+## Perform MAGMA on all trait folder in directory
+#######
+
+# Number of cores
+ncores=20
+
+# Experimental info (update for single or paired reads, and readlength). Set paired to "yes" if appropriate. NOTE: reads are assumed to be stranded.
+workDir="/share/ScratchGeneral/scoyou/projects/dysplasia_traits/skeletal_traits"
+geneLoc="/share/ScratchGeneral/scoyou/projects/dysplasia_GWAS/MAGMA_files/NCBI37/NCBI37.3.gene.loc"
+genome="/share/ScratchGeneral/scoyou/projects/dysplasia_GWAS/MAGMA_files/g1000_eur/g1000_eur"
+geneList="/share/ScratchGeneral/scoyou/projects/dysplasia_GWAS/gene_sets/Dysplasia_ENTREZ.txt"
+
+# Path to folder containing sample folders
+sample_Path=$workDir
+echo "sample_Path "$sample_Path
+
+# Make an array containing names of directories containing samples
+sample_arr=( $(ls $workDir) )
+echo "# in samples array ${#sample_arr[@]}"
+echo "names in samples array ${sample_arr[@]}"
+
+# Submit command for each sample in array
+for sample in ${sample_arr[@]}; do
+
+# Runs loop for only the first sample in array (used for development)
+# for sample in ${sample_arr[0]}; do
+
+# Define input directory, define and make output and log directories
+inPath=$sample_Path/$sample
+echo "inPath $inPath"
+
+# Path for log files
+logDir=$sample_Path/$sample
+mkdir -p $logDir
+echo "logDir $logDir"
+
+# Define input files
+inFile=$inPath/$sample/*"_MAGMA.input.txt"
+echo "inFile1 $inFile1"
+
+# Command to be executed
+Command="/home/scoyou/projects/dysplasia_traits/dysplasia_traits_scripts/MAGMA_each.sh $inPath $inFile $geneLoc $sample $genome $geneList"
+
+# Submit to queue
+echo "Command "$Command
+# $CommandPE
+# qsub -P OsteoporosisandTranslationalResearch -N 'MAGMA'$sample -b y -wd $logDir -j y -R y -l mem_requested=8G -pe smp $ncores -V -m bea -M s.youlten@garvan.org.au $Command
+done
